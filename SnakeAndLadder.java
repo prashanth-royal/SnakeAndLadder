7054e8c8d92c31c46d8e08304184ab72650b283c
Prashanth <prashanth.royal2046@gmail.com>
2021-05-09 10:40:50 +0530

    Added: Check for option

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,6 +4,24 @@ public class SnakeAndLadder {
 
 		int randomDieNum = (int)(Math.random()*6) + 1;
 		System.out.println("Random Die is: " + randomDieNum);
+
+		//constants
+		int NoPlay=0;
+      int Ladder=1;
+      int Snake=2;
+
+		//check for options like NoPlay Ladder and Snake
+		int Option = (int)(Math.random()*3);
+		System.out.println("Random number is: " + Option);
+
+		//to check for the player position by if elseif statement
+
+		if (Option == Ladder)
+			System.out.println("Player Position is: " + randomDieNum);
+		else if (Option == Snake)
+			System.out.println("Player position is: " + -randomDieNum);
+		else
+			System.out.println("Player position is: " + 0);
 	}
 }
 
